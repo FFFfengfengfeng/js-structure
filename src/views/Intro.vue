9eaf600aebf73ef2167882e8b73c112f2bd94290
FFFfengfengfeng <1098865109@qq.com>
2018-06-12 00:28:10 +0800

    no message

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -4,6 +4,8 @@
         <h4>环境搭建</h4>
         <p>javascript不像java,python,c等语言,需要自己配置开发环境,运行javascript只需要一个浏览器(Chrome或者Firefox)是不错的选择;</p>
         <p>但浏览器运行javascript(以下简称: js)并不是唯一的路径</p>
+        <p>可以使用web服务器运行javascript(如: xampp)</p>
+        <p>还可以使用nodejs搭建web服务器</p>
     </div>
 </template>
 
