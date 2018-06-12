d781b642c8b92037bd320cef47f7d3974867a46e
FFFfengfengfeng <1098865109@qq.com>
2018-06-12 10:23:02 +0800

    变量,标题样式

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="container">
-        <h3>第一章</h3>
-        <h4>环境搭建</h4>
-        <p>javascript不像java,python,c等语言,需要自己配置开发环境,运行javascript只需要一个浏览器(Chrome或者Firefox)是不错的选择;</p>
-        <p>但浏览器运行javascript(以下简称: js)并不是唯一的路径</p>
+        <h2>第一章</h2>
+        <h3>环境搭建</h3>
+        <p>javascript不像java,python,c等语言,需要自己配置开发环境,运行javascript非常简单</p>
+        <p>通过浏览器控制台运行(chrome或firefox)</p>
         <p>可以使用web服务器运行javascript(如: xampp)</p>
         <p>还可以使用nodejs搭建web服务器</p>
+        <h3>Javascript基础</h3>
+        <p>在html中编写js有两种方式:</p>
+        <p>通过<strong>&lt;script&gt;&lt;/script&gt;</strong>标签,将js代码都写在这个标签里</p>
+        <p>第二种方式可以用指定&lt;script&gt;的<strong>src</strong>属性,使用外部链接的js脚本</p>
+        <h4>变量</h4>
+        <p>变量</p>
     </div>
 </template>
 
