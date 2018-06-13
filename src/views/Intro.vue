f07bddcd56c3428f01f74e21da5aaac71cd94496
FFFfengfengfeng <1098865109@qq.com>
2018-06-13 10:53:18 +0800

    全局作用域

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -11,7 +11,11 @@
         <p>通过<strong>&lt;script&gt;&lt;/script&gt;</strong>标签,将js代码都写在这个标签里</p>
         <p>第二种方式可以用指定&lt;script&gt;的<strong>src</strong>属性,使用外部链接的js脚本</p>
         <h4>变量</h4>
-        <p>变量</p>
+        <p>js有多种数据类型,string, number, bolean, object, null, undefined; es6中新加了Symbol的数据类型</p>
+        <p>虽然js有多种数据类型,但js声明变量并不需要声明类型</p>
+        <h4>变量作用域</h4>
+        <p>js中没有块级作用域,但有函数作用域,同时有一个全局作用域,所有函数都能访问全局作用域,
+            但全局作用域并不能访问函数中声明的变量,开发当中应该尽量减少使用全局作用域,因为这样会造成全局污染</p>
     </div>
 </template>
 
